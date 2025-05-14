8bb0c8e018e8f2e00fab0fbcd555f030226696b9
mahdi Aleboyeh <m.a8@mahdis-MacBook-Air.local>
2025-05-14 13:18:32 +0330

    first pushing on github

---
+++ playground/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=255, blank=True, null=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='media/posts/')
+    published_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    like_dislike = models.BooleanField(default=True)
+    
