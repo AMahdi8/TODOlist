f9645ea294a7173e0051e0e661bec1727853ee20
mahdi Aleboyeh <m.a8@mahdis-MacBook-Air.local>
2025-05-14 13:19:53 +0330

    add __str__ to Post model

--- playground/models.py
+++ playground/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     phone_number = models.CharField(max_length=11)
 
+
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255, blank=True, null=True)
     body = models.TextField()
     image = models.ImageField(upload_to='media/posts/')
@@ -13,7 +16,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
+
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes')
     like_dislike = models.BooleanField(default=True)
-    
